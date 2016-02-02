0a519f3daaf90cfe9489c5bcc7c61b6b9a457121
Rémi Courtel <remi.courtel@gmail.com>
2016-02-02 21:55:29 +0100

    added spotify authentication

--- SpotyScraper.Spotify/SpotifyService.cs
+++ SpotyScraper.Spotify/SpotifyService.cs
@@ -1,5 +1,7 @@
 ﻿using SpotifyAPI.Web;
+using SpotifyAPI.Web.Auth;
 using SpotifyAPI.Web.Enums;
+using SpotifyAPI.Web.Models;
 using SpotyScraper.Model.StreamServices;
 using SpotyScraper.Model.Tracks;
 using System;
@@ -8,6 +10,7 @@ using System.ComponentModel.Composition;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace SpotyScraper.Spotify
@@ -20,25 +23,79 @@ namespace SpotyScraper.Spotify
         public const string NAME = "Spotify";
         public const string DESCRIPTION = "Spotify streaming service";
 
-        private readonly SpotifyWebAPI _spotify;
-
-        public SpotifyService()
-        {
-            _spotify = new SpotifyAPI.Web.SpotifyWebAPI();
-        }
-
         public string Name { get; } = NAME;
 
         public string Description { get; } = DESCRIPTION;
 
+        #region Authentication
+
+        private const int PORT = 30968;
+        private const string CLIENTID = "d49d7e00e3eb448784c74177a0254656";
+
+        private readonly string _state = new Random().Next(int.MinValue, int.MaxValue).ToString();
+        private readonly SemaphoreSlim _authenticationSemaphore = new SemaphoreSlim(0, 1);
+
+        private SpotifyWebAPI _spotify;
+        private ImplicitGrantAuth _authentication;
+
+        private async Task<bool> CheckAuthentication()
+        {
+            this.StartAuthentication();
+            await _authenticationSemaphore.WaitAsync();
+            return _spotify != null;
+        }
+
+        private void StartAuthentication()
+        {
+            _authentication = new ImplicitGrantAuth
+            {
+                RedirectUri = $"http://localhost:{PORT}",
+                ClientId = CLIENTID,
+                Scope = Scope.PlaylistReadPrivate,
+                State = _state,
+            };
+            _authentication.OnResponseReceivedEvent += _authentication_OnResponseReceivedEvent; ;
+
+            _authentication.StartHttpServer(PORT);
+            _authentication.DoAuth();
+        }
+
+        private void _authentication_OnResponseReceivedEvent(Token token, string state)
+        {
+            _authentication.StopHttpServer();
+
+            try
+            {
+                if (state != _state)
+                    throw new InvalidOperationException($"{nameof(SpotifyService)} - Wrong state received");
+
+                if (token.Error != null)
+                    throw new InvalidOperationException($"{nameof(SpotifyService)} - Error: {token.Error}");
+
+                _spotify = new SpotifyWebAPI
+                {
+                    UseAuth = true,
+                    AccessToken = token.AccessToken,
+                    TokenType = token.TokenType
+                };
+            }
+            finally
+            {
+                _authenticationSemaphore.Release();
+            }
+        }
+
+        #endregion Authentication
+
         public async Task ResolveAsync(IEnumerable<Track> tracks)
         {
-            //var tasks = new List<Task>();
+            if (!await this.CheckAuthentication())
+                return;
+
             foreach (var track in tracks)
             {
                 await this.ResolveAsync(track);
             }
-            //await Task.WaitAll(tasks.ToArray());
         }
 
         public async Task ResolveAsync(Track track)
