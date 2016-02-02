5e19988cda27be16f7a64d984152ad8e41b4b54a
Rémi Courtel <remi.courtel@gmail.com>
2016-02-02 21:52:59 +0100

    added minimal exception handler

--- SpotyScraper.View/App.xaml.cs
+++ SpotyScraper.View/App.xaml.cs
@@ -6,6 +6,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace SpotyScraper.View
 {
@@ -19,6 +20,28 @@ namespace SpotyScraper.View
             base.OnStartup(e);
 
             DispatcherHelper.Initialize();
+
+            this.DispatcherUnhandledException += App_DispatcherUnhandledException;
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+        }
+
+        private void App_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            this.OnUnhandledException(e.Exception);
+        }
+
+        private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            this.OnUnhandledException(e.ExceptionObject as Exception);
+        }
+
+        private void OnUnhandledException(Exception exception)
+        {
+            MessageBox.Show(
+                $"An error occured:{Environment.NewLine}{exception.GetBaseException().GetType().Name}: {exception.GetBaseException().Message}",
+                "Oops, an error occured",
+                MessageBoxButton.OK,
+                MessageBoxImage.Error);
         }
     }
 }
